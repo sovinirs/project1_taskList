058fddf3f9c17262221abd30df45b46908f7ed0f
vijay.srinivasan1998@gmail.com <vijay.srinivasan1998@gmail.com>
2020-01-21 23:50:37 +0530

    Clear List Changes to app.js

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const tasks = document.querySelector(".collection");
 const addTask = document.querySelector(".add-task");
 const clearTasks = document.querySelector(".clear-tasks");
 const taskValue = document.querySelector("#task");
+const clearEvents = document.querySelector(".clear-tasks");
 
 addTask.addEventListener('click', addTaskEvent);
 tasks.addEventListener('click', removeTaskEvent);
+clearEvents.addEventListener('click', clearEvent);
 
 function addTaskEvent(e){
   if(taskValue.value === ''){
@@ -30,13 +32,21 @@ function addTaskEvent(e){
   }
 }
 
-//Removing the element
+// Removing the element
 function removeTaskEvent(e){
   if(e.target.parentElement.classList.contains("delete-item")){
-    if(confirm("Are You Sure ?")){
+    if(confirm("Are You Sure you want to remove the selected task item ?")){
       e.target.parentElement.parentElement.remove();
     }
   }
+}
+
+// Clear Event
+function clearEvent(e){
+  if(confirm("Are you sure you want to clear the tasks ?")){
+    tasks.children.remove();
+  }
+  e.preventDefault();
 }
 
 console.log("Success");
